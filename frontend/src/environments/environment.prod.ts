6847c2fc3b0d9e50c93ca240ca973c5cef8de190
DAO Hamadou <daohamadou@gmail.com>
2024-06-03 12:19:13 +0000

    -

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,8 +1,14 @@
+const host = location.hostname
+const adresse = location.origin + '/backend/public/index.php/';
+const url: string = location.origin.includes('geschool.barasoft-burkina.com')? adresse: '/';
 export const environment = {
     production: true,
-     urlApi:"http://51.75.127.50:81/piva-backend/",  
-     // urlApi:"http://localhost/backend/public/index.php/",
+    // urlApi:"http://localhost/backend/public/index.php/",
+    // urlApi:"http://"+host+"/backend/public/index.php/",
+    // urlApi:"https://backend.geschool.barasoft-burkina.com/public/index.php/",
     // urlApi:"http://51.178.18.128:8201/"
+    // urlApi:"http://geschool-backend.ddns.net/",
     // urlApi:"/"
-    // urlApi:"http://localhost:8000/"
+    // urlApi: url
+    urlApi: adresse
 };
